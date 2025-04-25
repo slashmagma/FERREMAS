604cdbead659472754626f2e717b7ecf8a9f23e4
Slashking <nicothehedgehog1@gmai.com>
2025-04-25 17:33:00 -0400

    segundo comit

---
+++ task/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+
+# Create your views here.
+def Home(request):
+    return render(request, 'Home.html', {
+        
+    })
+def singup(request):
+
+    if request.method == 'GET':
+        return render(request, 'singup.html', {
+        'form': UserCreationForm()
+        })
+        
+    else:
+      if request.POST['password1'] == request.POST['password2']:
+      #registra usario
+          user = User.objects.create_user(
+          username=request.POST['username'],
+          password=request.POST['password1'],)
+          user.save()
+          
+      
+    return HttpResponse('password no coinciden')
+
+
+   
+    
